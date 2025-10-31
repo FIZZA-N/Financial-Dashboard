e055952e8a2e56b384b39ba50bb52ed18c85f653
Dijination-Saad <M.Saad@dijination.net>
2025-11-01 02:35:26 +0500

    Update server.js

--- my-app/server.js
+++ my-app/server.js
@@ -43,7 +43,7 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/summary', summaryRoutes);
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
